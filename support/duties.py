99f1e4d639c70bb7e3f44ef3bf0254113062d74b
Thomas McQuitty <tm633717@cyfm66wwh9.wifi.broadcom.net>
2026-08-19 07:39:03 -0400

    Update from template

---
+++ support/duties.py
@@ -0,0 +1,36 @@
+import sys; sys.path.append('./')
+
+from duty import duty
+from zowesupport import *
+
+# Commands to Run
+
+    
+@duty
+def run(ctx):
+    """Run Bind and Grant Jobs"""
+    dataset = f"{config.runJCL}"
+    submitJobAndDownloadOutput(ctx, dataset, "job-archive", 0)
+
+@duty
+def build_cobol(ctx):
+    """Build Cobol Element"""
+    command = f"zowe endevor generate element {config.element} --type COBOL --os --maxrc 0 --sn 1 --cb"
+    simpleCommand(ctx, command, "output")
+
+@duty
+def build_lnk(ctx):
+    """Build LNK Element"""
+    command = f"zowe endevor generate element {config.element} --type LNK --os --maxrc 0 --sn 1 --cb"
+    simpleCommand(ctx, command, "output")
+
+@duty
+def build(ctx):
+    """Build the application"""
+    build_cobol(ctx)
+    build_lnk(ctx)
+
+@duty
+def clean(ctx):
+    """Clean up temp files"""
+    ctx.run("rm -rf endevor*.txt output")
